94772663a13ae68ad480304d5b7461246ddf8876
abdullahallnaim <naim1823007@stud.kuet.ac.bd>
2023-06-16 11:01:20 +0600

    done

---
+++ accounts/models.py
@@ -0,0 +1,79 @@
+from decimal import Decimal
+
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import (
+    MinValueValidator,
+    MaxValueValidator,
+)
+from django.db import models
+
+from .constants import GENDER_CHOICE
+from .managers import UserManager
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, null=False, blank=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    @property
+    def balance(self):
+        if hasattr(self, 'account'):
+            return self.account.balance
+        return 0
+
+
+class BankAccountType(models.Model):
+    name = models.CharField(max_length=128)
+    maximum_withdrawal_amount = models.DecimalField(
+        decimal_places=2,
+        max_digits=12
+    )
+    def __str__(self):
+        return self.name
+
+
+class UserBankAccount(models.Model):
+    user = models.OneToOneField(
+        User,
+        related_name='account',
+        on_delete=models.CASCADE,
+    )
+    account_type = models.ForeignKey(
+        BankAccountType,
+        related_name='accounts',
+        on_delete=models.CASCADE
+    )
+    account_no = models.PositiveIntegerField(unique=True)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICE)
+    birth_date = models.DateField(null=True, blank=True)
+    balance = models.DecimalField(
+        default=0,
+        max_digits=12,
+        decimal_places=2
+    )
+    initial_deposit_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return str(self.account_no)
+
+
+
+class UserAddress(models.Model):
+    user = models.OneToOneField(
+        User,
+        related_name='address',
+        on_delete=models.CASCADE,
+    )
+    street_address = models.CharField(max_length=512)
+    city = models.CharField(max_length=256)
+    postal_code = models.PositiveIntegerField()
+    country = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.user.email
---
+++ transactions/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+from .constants import TRANSACTION_TYPE_CHOICES
+from accounts.models import UserBankAccount
+
+class Transaction(models.Model):
+    account = models.ForeignKey(
+        UserBankAccount,
+        related_name='transactions',
+        on_delete=models.CASCADE,
+        null=True, blank=True
+    )
+    amount = models.DecimalField(
+        decimal_places=2,
+        max_digits=12,
+        null= True
+    )
+    balance_after_transaction = models.DecimalField(
+        decimal_places=2,
+        max_digits=12,
+        null = True
+    )
+    transaction_type = models.PositiveSmallIntegerField(
+        choices=TRANSACTION_TYPE_CHOICES,
+        null = True
+    )
+    timestamp = models.DateTimeField(auto_now_add=True)
+    loan_approved = models.BooleanField(default=False)
+    # def __str__(self):
+    #     return str(self.account.account_no)
+    class Meta:
+        ordering = ['timestamp']
